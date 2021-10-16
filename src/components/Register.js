62c4a720596edff93b375adf90e9e72a8052f903
SagarGi <sagargurung1001@gmail.com>
2021-10-16 11:22:11 +0545

    register-front-midway

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,157 +1,319 @@
-import userEvent from '@testing-library/user-event';
-import React from 'react'
-import {Redirect, useHistory } from 'react-router-dom';
-import Select from 'react-select';
-import axios from 'axios';
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { Redirect, useHistory } from "react-router-dom";
+import Select from "react-select";
+import axios from "axios";
+import "../cssfolder/register.css";
 
 const ieltsOptions = [
-  { value: 'no', label: 'no' },
-  { value: 'yes', label: 'yes' },
-  
+  { value: "no", label: "no" },
+  { value: "yes", label: "yes" },
 ];
 
 const destinationOptions = [
-  { value: 'australia', label: 'australia' },
-  { value: 'usa', label: 'usa' },
-  { value: 'canada', label: 'canada' },
-  { value: 'japan', label: 'japan' },
-  
+  { value: "australia", label: "australia" },
+  { value: "usa", label: "usa" },
+  { value: "canada", label: "canada" },
+  { value: "japan", label: "japan" },
 ];
 
 const qualificationOptions = [
-  { value: '+2', label: '+2' },
-  { value: 'bachelors', label: 'bachelors' },
-  { value: 'masters', label: 'masters' },]
-  ;
-
-
-
+  { value: "+2", label: "+2" },
+  { value: "bachelors", label: "bachelors" },
+  { value: "masters", label: "masters" },
+];
 class Register extends React.Component {
-  constructor(props)
-  {
-    super(props)
-  
+  constructor(props) {
+    super(props);
+
     this.state = {
-      ielts:  { value: 'no', label: 'no' },
-      destination:  { value: 'australia', label: 'australia' },
-      qualification:{ value: '+2', label: '+2' },
+      ielts: { value: "no", label: "no" },
+      destination: { value: "australia", label: "australia" },
+      qualification: { value: "+2", label: "+2" },
       showOption: false,
-      name:"",
-      address:"",
-      phone:"",
-      email:"",
-      percentage:"",
-      listening:"",
-      reading:"",
-      writing:"",
-      speaking:"",
-      overallband:"",
+      name: "",
+      address: "",
+      phone: "",
+      email: "",
+      percentage: "",
+      listening: "",
+      reading: "",
+      writing: "",
+      speaking: "",
+      overallband: "",
     };
- }
-
-
- handleDestinationChange = destination => {
-   
-  this.setState({ destination });
-};
-
-handleQualificationChange = qualification => {
-   
-  this.setState({ qualification });
-};
-
-
-
-
- handleIeltsChange = ielts => {
-   
-  this.setState({ ielts });
-
-  if(ielts.value === "yes")
-  {
-    this.setState({
-      showOption:true
-    })
-  }else{
-    this.setState({
-      showOption:false
-    })
   }
-};
 
-//required function
+  handleDestinationChange = (destination) => {
+    this.setState({ destination });
+  };
 
-handleCancel = (e) => {
-  
-  this.props.history.push("/home");
-  
-}
+  handleQualificationChange = (qualification) => {
+    this.setState({ qualification });
+  };
 
-handleDataEntry = async () => {
-  // this.props.history.push("/home");
-  // e.preventDefault();
+  handleIeltsChange = (ielts) => {
+    this.setState({ ielts });
 
-  console.log(this.state.ielts.value)
+    if (ielts.value === "yes") {
+      this.setState({
+        showOption: true,
+      });
+    } else {
+      this.setState({
+        showOption: false,
+      });
+    }
+  };
 
-  const ielts = this.state.ielts.value
-  const destination =  this.state.destination.value
-  const qualification = this.state.qualification.value
+  //required function
 
-  const {name,address,phone,email,percentage,listening,reading,writing,speaking,overallband} = this.state
+  handleCancel = (e) => {
+    this.props.history.push("/home");
+  };
 
-  
+  handleDataEntry = async () => {
+    // this.props.history.push("/home");
+    // e.preventDefault();
 
-    const res = await fetch("/register",{
-      method:"POST",
-      headers:{
-        'Content-Type' : 'application/json'
+    console.log(this.state.ielts.value);
+
+    const ielts = this.state.ielts.value;
+    const destination = this.state.destination.value;
+    const qualification = this.state.qualification.value;
+
+    const {
+      name,
+      address,
+      phone,
+      email,
+      percentage,
+      listening,
+      reading,
+      writing,
+      speaking,
+      overallband,
+    } = this.state;
+
+    const res = await fetch("/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify({
-        name:name,
-        email:email,
-        phone:phone,
-        destination:destination,
-        qualification :qualification,
-        address:address,
-        percentage:percentage,
-        ielts:ielts,
-        listening:listening,
-        reading:reading,
-        writing:writing,
-        speaking:speaking,
-        overallband:overallband
-      })
-    })
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        phone: phone,
+        destination: destination,
+        qualification: qualification,
+        address: address,
+        percentage: percentage,
+        ielts: ielts,
+        listening: listening,
+        reading: reading,
+        writing: writing,
+        speaking: speaking,
+        overallband: overallband,
+      }),
+    });
 
     const data = await res.json();
-    if(data.status === 422 || !data)
-    {
+    if (data.status === 422 || !data) {
       window.alert("Failed");
       console.log("Registration failed");
-    }else{
+    } else {
       window.alert("Successfull !!!");
       this.props.history.push("/home");
       console.log("Registration successfull");
     }
+  };
 
-   
-}
-
-  render()
-  {
-
-    
-    if(localStorage.getItem("token") === null)
-    {
-      return <Redirect to="/" />
+  render() {
+    if (localStorage.getItem("token") === null) {
+      return <Redirect to="/" />;
     }
 
-    const { ielts, destination , qualification } = this.state;
-    return(
-      <div>
+    const { ielts, destination, qualification } = this.state;
+    return (
+      <div className="container main-register-div">
+        <div className = "register-heading-div text-center">
+          <h2 className = "register-heading">Register New Student</h2>
+        </div>
+        <div className="student-info-div">
+          <div className="row">
+            <div className="col-6" style = {{paddingLeft:"130px"}}>
+              <lable>
+                Name: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
 
-       
-        <input type = "text"  onChange={(e) => { this.setState({name: e.target.value})}}
+            <div className="col-6">
+              <lable>
+                Email: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+          </div>
+
+          <div className="row"  style = {{marginTop:"30px"}}>
+            <div className="col-6" style = {{paddingLeft:"130px"}}>
+              <lable>
+                Address: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+
+            <div className="col-6" >
+              <lable>
+                Phone:
+                <span style={{ color: "red", textIndent: "2em" }}>*</span>
+              </lable>
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+          </div>
+
+          <div className="row"  style = {{marginTop:"30px"}}>
+            <div className="col-6" style = {{paddingLeft:"130px"}}>
+              <lable>
+                Address: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+
+            <div className="col-6" >
+              <lable>
+                :
+                <span style={{ color: "red", textIndent: "2em" }}>*</span>
+              </lable>
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="container register-heading-div">
+          <h2 className="register-heading">Register New Student</h2>
+        </div>
+
+        <div className="container student-info-div">
+          <div className="row">
+            <div className="col-6" style = {{paddingLeft:"180px"}}>
+              <lable>
+                Name: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+
+            <div className="col-6" style = {{paddingLeft:"180px"}}>
+              <lable>
+                Email: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+          </div>
+
+          <div className="row"  style = {{marginTop:"30px"}}>
+            <div className="col-6"  style = {{paddingLeft:"180px"}}>
+              <lable>
+                Address: <span style={{ color: "red" }}>*</span>
+              </lable>{" "}
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+
+            <div className="col-6"  style = {{paddingLeft:"180px"}}>
+              <lable>
+                Phone:
+                <span style={{ color: "red", textIndent: "2em" }}>*</span>
+              </lable>
+              <br></br>
+              <input
+                className="name-input"
+                type="text"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                placeholder=""
+              />
+            </div>
+          </div>
+        </div> */}
+      </div>
+    );
+
+    {
+      /* <input type = "text"  onChange={(e) => { this.setState({name: e.target.value})}}
           
           placeholder = "Enter your name" /> <br></br>
 
@@ -196,11 +358,15 @@ handleDataEntry = async () => {
             options={ieltsOptions}
             isSearchable={false}
             
-          />
+          /> */
+    }
 
-          {/* invisible div when option yes is clicked!! */}
+    {
+      /* invisible div when option yes is clicked!! */
+    }
 
-          <div>
+    {
+      /* <div>
           {this.state.showOption &&
            <div>
              <p>Please enter your ielts score</p>
@@ -216,11 +382,8 @@ handleDataEntry = async () => {
           </div>
 
           <button onClick = {this.handleDataEntry}>Save</button> <button onClick ={() => this.handleCancel()} >Cancel</button>
-     
-
-        
-      </div>
-    )
+      */
+    }
   }
 }
 
